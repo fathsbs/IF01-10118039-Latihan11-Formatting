4def50d5df141d5647205eb34723ba3b22e072fe
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:11:40 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan11-Formatting/src/IF0110118039Latihan11Formatting.java
@@ -0,0 +1,31 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+
+}
